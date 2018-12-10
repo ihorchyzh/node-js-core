78d789c9c9b59949930ac9c86ac3cd92e628077e
Ihor Chyzh <ichyzh@gmail.com>
2018-12-11 00:23:25 +0200

    Update the path

--- 002-require/app.js
+++ 002-require/app.js
@@ -1,8 +1,11 @@
-console.log('Starting app.');
+console.log('Starting app...');
 
 const fs = require('fs');
 const os = require('os');
 
 var user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`);
+fs.appendFile('./002-require/greetings.txt', `Hello ${user.username}!`, (err) => {
+    if (err) throw err;
+    console.log('The "data to append" was appended to file!');
+});
